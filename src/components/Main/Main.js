c35f1948482c6522d5bcce6863c249eefed024a5
Steven McKeown <stevenmckeown19@gmail.com>
2022-05-25 17:24:29 +0100

    Added function to randomize actor positions on card click

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 
 const Main = () => {
-    const [actor] = useState([
+    const [actor, setActor] = useState([
         {
             id: 1,
             firstName: "Brad",
@@ -29,16 +29,31 @@ const Main = () => {
     const [score, setScore] = useState(0);
     const [bestScore, setBestScore] = useState(0);
 
+    /**
+     * If the actorStore array does not include the name of the actor that was clicked, add the name of
+     * the actor to the actorStore array, increment the score by 1, and if the score is greater than or
+     * equal to the bestScore, set the bestScore to the score plus 1. If the actorStore array does
+     * include the name of the actor that was clicked, log an error, set the score to 0, and set the
+     * actorStore array to an empty array
+     * @param e - the event that is triggered when the image is clicked
+     */
     const handleActorClick = (e) => {
         if (!actorStore.includes(e.target.name)) {
             setActorStore(actorStore.concat(e.target.name));
             setScore(score + 1);
-            score >= bestScore ? setBestScore(score + 1) : console.log("error");
+
+            if (score >= bestScore) setBestScore(score + 1);
         } else {
-            console.log("error");
+            console.log("You already clicked this! Try again!");
             setScore(0);
             setActorStore([]);
         }
+    };
+
+    const randomize = () => {
+        document.querySelector(".card").addEventListener("click", () => {
+            setActor(actor.sort(() => Math.random() - 0.5));
+        });
     };
 
     return (
@@ -48,7 +63,7 @@ const Main = () => {
             </p>
             {actor.map((item) => {
                 return (
-                    <div key={item.id}>
+                    <div key={item.id} className="card" onClick={randomize}>
                         <Card
                             name={item.firstName}
                             img={item.imgUrl}
